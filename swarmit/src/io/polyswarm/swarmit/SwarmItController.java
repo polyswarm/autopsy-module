c6d1ad5ba71a53355041fb6dc8302265e4087082
Nick Davis <nick.davis@polyswarm.io>
2018-10-16 17:15:50 -0400

    added license to all file headers.

--- swarmit/src/io/polyswarm/swarmit/SwarmItController.java
+++ swarmit/src/io/polyswarm/swarmit/SwarmItController.java
@@ -1,7 +1,25 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * The MIT License
+ *
+ * Copyright 2018 PolySwarm PTE. LTD.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
  */
 package io.polyswarm.swarmit;
 
@@ -9,8 +27,8 @@ import com.google.common.util.concurrent.ListeningScheduledExecutorService;
 import com.google.common.util.concurrent.MoreExecutors;
 import com.google.common.util.concurrent.ThreadFactoryBuilder;
 import io.polyswarm.swarmit.apiclient.SwarmItApiClient;
-import io.polyswarm.swarmit.apiclient.SwarmItVerdictEnum;
 import io.polyswarm.swarmit.apiclient.SwarmItVerdict;
+import io.polyswarm.swarmit.apiclient.SwarmItVerdictEnum;
 import io.polyswarm.swarmit.datamodel.SwarmItDb;
 import io.polyswarm.swarmit.datamodel.SwarmItDbException;
 import io.polyswarm.swarmit.datamodel.SwarmItPendingSubmission;
@@ -28,8 +46,6 @@ import javafx.beans.property.SimpleDoubleProperty;
 import javafx.beans.property.SimpleObjectProperty;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.concurrent.Worker;
-import org.apache.http.client.ClientProtocolException;
-import org.json.JSONException;
 import org.json.JSONObject;
 import org.netbeans.api.progress.ProgressHandle;
 import org.openide.util.Cancellable;
--- swarmit/src/io/polyswarm/swarmit/apiclient/SwarmItApiClient.java
+++ swarmit/src/io/polyswarm/swarmit/apiclient/SwarmItApiClient.java
@@ -1,10 +1,30 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * The MIT License
+ *
+ * Copyright 2018 PolySwarm PTE. LTD.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
  */
 package io.polyswarm.swarmit.apiclient;
 
+import static io.polyswarm.swarmit.apiclient.SwarmItJsonHelper.getJSONObjectFromResult;
+import static io.polyswarm.swarmit.apiclient.SwarmItJsonHelper.getUUIDFromResult;
 import io.polyswarm.swarmit.optionspanel.SwarmItMarketplaceSettings;
 import java.io.IOException;
 import java.net.URI;
@@ -27,11 +47,8 @@ import org.apache.http.util.EntityUtils;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
-import org.json.JSONTokener;
 import org.sleuthkit.datamodel.AbstractFile;
 import org.sleuthkit.datamodel.ReadContentInputStream;
-import static io.polyswarm.swarmit.apiclient.SwarmItJsonHelper.getUUIDFromResult;
-import static io.polyswarm.swarmit.apiclient.SwarmItJsonHelper.getJSONObjectFromResult;
 
 /**
  * Make requests to PolySwarm API and manage responses and parse responses.
